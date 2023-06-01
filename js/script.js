4c24c4b81f4626fdf4151a1f296e3245c0470f59
MUHAMMAD MAROOF <mmaroof487@gmail.com>
2023-06-01 21:10:17 +0530

    feat: added function to filter

--- js/script.js
+++ js/script.js
@@ -1,61 +1,118 @@
-const x = document.getElementById("filter");
-const y = document.getElementById("anime");
-const z = document.getElementById("opac")
-const b = document.getElementById("list-places");
-const c = document.getElementById("list-guests");
-const h = document.getElementById("list-search");
+const filter = document.getElementById("filter");
+const anime = document.getElementById("anime");
+const main = document.getElementById("main")
+const listPlaces = document.getElementById("list-places");
+const listGuests = document.getElementById("list-guests");
+const listSearch = document.getElementById("list-search");
 
-// filter show 
-x.onclick = () => {
- y.style.animation = "filter .5s forwards"
- z.style.opacity= ".7";
- h.innerHTML= " <img src=\"assets/images/search2.png\"/> " +  " Search"
+// filter appear
+filter.onclick = () => {
+    anime.style.animation = "filter .5s forwards"
+    main.style.opacity = ".7";
+    listSearch.innerHTML = `<img src="assets/images/search2.png"/> Search`
 };
 // list opacity 
-b.onclick = () => {
-    b.style.opacity= "1";
-    c.style.opacity=".5"
+listPlaces.onclick = () => {
+    listPlaces.style.opacity = "1";
+    listGuests.style.opacity = ".5"
 }
-c.onclick = () => {
-    c.style.opacity= "1";
-    b.style.opacity=".5"
+listGuests.onclick = () => {
+    listGuests.style.opacity = "1";
+    listPlaces.style.opacity = ".5"
 }
 // filter vanish 
-h.onclick ,z.onclick  = () => {
-if( h.innerHTML= " <img src=\"assets/images/search2.png\"/> " +  " Search"){
-        z.style.opacity= "1"; 
-        y.style.animation = "rev-filter .5s forwards"
+main.onclick = () => {
+    if (listSearch.innerHTML = `<img src="assets/images/search2.png"/> Search`) {
+        main.style.opacity = "1";
+        anime.style.animation = "rev-filter .5s forwards"
     }
 }
 
 // location set 
-const d = document.getElementById("location-click");
-const e = document.getElementById("locations");
-d.onclick = () => {
-    let m = d.innerHTML;
-    e.innerHTML= "Location set to : " + m;
-};
+const locationClick = document.getElementsByClassName("location-click");
+const locations = document.getElementById("locations");
+
+for (let i = 0; i < locationClick.length; i++) {
+    locationClick[i].onclick = () => {
+    let m = locationClick[i].innerHTML;
+    locations.innerHTML = `Location set to :  ${m}`;
+ TODO:locationClick[i].innerHTML = "J&K";  
+   }
+}
+
 // guest set 
-const f= document.getElementById("guest-click");
-const g = document.getElementById("guests");
-f.onclick = () => {
-    let n = f.innerHTML;
-    g.innerHTML= "Guests : " + n;
-};
-// search function 
-h.onclick  = () => {
-    if ( e.innerHTML.length > 17 && g.innerHTML.length > 9 ){
-            z.style.opacity= "1"; 
-            y.style.animation = "rev-filter .5s forwards"
-    }else{
-        alert("Please select")
+const guestClick = document.getElementsByClassName("guest-click");
+const guests = document.getElementById("guests");
+
+for (let i = 0; i < guestClick.length; i++) {
+    guestClick[i].onclick = () => {
+    let m = guestClick[i].innerHTML;
+    guests.innerHTML = `Location set to :  ${m}`;
     }
 }
 
-    
-// card click 
-const a = document.getElementById("card");
-a.onclick = function() {
-    console.log("card clicked");
-    a.style.animation = "whoosh 1s forwards"
-};
+// search function 
+const card = document.getElementsByClassName("card");
+const sgr = document.getElementsByClassName("sgr");
+const anant = document.getElementsByClassName("anant");
+const barm = document.getElementsByClassName("barm");
+const resultant = document.getElementById("resultant-stays");
+const result = document.getElementById("resultant-place");
+let sgrtest = /Srinagar/;
+let anantest = /Anantnag/;
+let baratest = /Baramulla/;
+
+listSearch.onclick = () => {
+    if (locations.innerHTML.length > 17 || guests.innerHTML.length > 9) {
+        main.style.opacity = "1";
+        anime.style.animation = "rev-filter .5s forwards";
+        for(let i = 0; i < card.length; i++){
+            card[i].style.display = "none";
+        }
+    }
+    if (sgrtest.test(locations.innerHTML)) {
+        for(let i = 0; i < sgr.length; i++){  
+           resultant.innerHTML = `${i + 1} stays`;
+           result.innerHTML = "Stays in Srinagar";
+           sgr[i].style.display = "block";
+        }
+    } else if (anantest.test(locations.innerHTML)){
+            for(let i = 0; i < anant.length; i++){  
+               resultant.innerHTML = `${i + 1} stays`;
+               result.innerHTML = "Stays in Anantnag";
+               anant[i].style.display = "block";
+            }
+    } else if (baratest.test(locations.innerHTML)){
+        for(let i = 0; i < barm.length; i++){  
+            resultant.innerHTML = `${i + 1} stays`;
+            result.innerHTML = "Stays in Baramullah";
+            barm[i].style.display = "block";
+         } 
+        } else {
+            for(let i = 0; i < card.length; i++){
+                card[i].style.display = "inline-block";
+            }
+         }
+    }
+
+
+// card click TODO:
+for(let i = 0; i < card.length; i++){
+    card[i].onclick = function () {
+        console.log("card clicked");
+        card[i].style.animation = "whoosh 1s forwards"
+    };
+}
+
+// nav change on scroll 
+let nav = document.getElementById("nav");
+document.onscroll = () => {
+    if (document.width > 1000){
+    if (document.documentElement.scrollTop > 60){
+        console.log("scrolled");
+        nav.style.padding = "30px 0"
+    }else{
+        nav.style.padding = "50px 0" 
+    }
+}
+}
